064cb4ef599c2566d61284d43c939561bb3663f1
Lefteris Chatziefraimidis <echatzief97@gmail.com>
2024-06-02 15:00:19 +0300

    initialized next js app with tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      description
+      <button className="btn">Button</button>
+      <label className="input input-bordered flex items-center gap-2">
+        Name
+        <input type="text" className="grow" placeholder="Daisy" />
+      </label>
+    </>
+  );
+}
